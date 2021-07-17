6d60c61320f77b94c0b258be5f0528f7b123a407
Diego Canez <canezdiego@gmail.com>
2021-07-16 21:47:55 -0500

    bucket_sort

--- ex1/measure_bucket.py
+++ ex1/measure_bucket.py
@@ -3,24 +3,31 @@ from subprocess import check_output
 
 TESTS = 1
 P_TESTS = [1, 2, 4, 8, 16]
-COMMAND = "mpirun --mca opal_warn_on_missing_libcuda 0 -np {0} ./bucket_sort.out"
+COMMAND = "mpirun --mca opal_warn_on_missing_libcuda 0 -np {0} ./bucket_sort"
 
 
 def measure_time(p):
     total = 0.0
+    comp = 0.0
+    comm = 0.0
 
     for i in range(TESTS):
         out = check_output(COMMAND.format(p).split()).decode("utf-8")
-        total += float(out.split()[1])
+        lines = out.splitlines()
+        total += float(out[0].split()[1])
+        comp += float(out[1].split()[1])
+        comm += float(out[2].split()[1])
 
     total /= TESTS
-    return total
+    comp /= TESTS
+    comm /= TESTS
+    return total, comp, comm
 
 
 def main():
     for p in P_TESTS:
-        test = measure_time(p)
-        print(f"{p}, {test}")
+        total, comp, comm = measure_time(p)
+        print(f"{p}, {total}, {comp}, {comm}")
 
 
 if __name__ == "__main__":
--- ex1/plot_bucket.py
+++ ex1/plot_bucket.py
@@ -1,10 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-x, y = np.loadtxt('data.csv', delimiter=',', unpack=True)
-plt.plot(x, y)
+p = []
+total = []
+comp = []
+comm = []
+
+with open('data.csv') as f:
+    rows = list(csv.reader(f, delimiter=','))
+    p = [int(row[0]) for row in rows]
+    total = [float(row[1]) for row in rows]
+    comp = [float(row[2]) for row in rows]
+    comm = [float(row[3]) for row in rows]
+
+print(p, total, comp, comm)
+plt.plot(p, total, label='Total time')
+plt.plot(p, comp, label='Computation time')
+plt.plot(p, comm, label='Communication time')
 
 plt.xlabel('Number of processes')
 plt.ylabel('Time')
 plt.title('Time vs number of processes')
+plt.legend()
 plt.savefig('bucket.png')
